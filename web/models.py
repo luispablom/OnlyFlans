03c22cc5f1c71e0ae0d4c54a35627cad64eb863b
luispablom <luispablomatamalabriones>
2024-09-18 20:58:06 -0300

    Primer commit: subiendo el proyecto OnlyFlans

---
+++ web/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import uuid
+from django.utils import timezone  # Importar timezone
+
+# Create your models here.
+class Flan(models.Model):
+    flan_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField()
+    slug = models.SlugField()
+    is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)  # Nuevo atributo
+
+    def __str__(self):
+        name = self.name
+        es_privado = self.is_private
+        return f'{name} - ¿Es privado?: {es_privado}'
+
+class Contact(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    mensaje = models.CharField(max_length=500)
+    
+    def __str__(self):
+        name = self.nombre
+        message = self.mensaje
+        return f'{name} : {message}'
